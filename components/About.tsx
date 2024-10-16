bd2b37e75abd9cf7ec76262ff5c89b0283b9486f
Sahil Sethi <sethisahil20132013@gmail.com>
2024-10-16 21:50:09 +0530

    started the About section/component

---
+++ components/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section>
+        <h2>
+            About me
+        </h2>
+        <p>
+            lorem ipsum
+        </p>
+        <p>
+            lorem ipsum
+        </p>
+    </section>
+  )
+}
