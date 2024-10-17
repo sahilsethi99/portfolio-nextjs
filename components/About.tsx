e3e527243fa8ca67d1a3d635a44a302e32a1c34a
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-18 05:18:37 +0530

    fixing stick footer

--- components/About.tsx
+++ components/About.tsx
@@ -1,17 +1,23 @@
+"use client";
+
 import React from 'react'
+import SectionHeading from './SectionHeading'
+import {motion} from 'framer-motion';
 
 export default function About() {
   return (
-    <section>
-        <h2>
-            About me
-        </h2>
+    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+    initial={{opacity: 0, y:100}}
+    animate={{opacity: 1, y:0}}
+    transition={{delay:0.175}}
+    >
+       <SectionHeading>About Me</SectionHeading>
         <p>
             lorem ipsum
         </p>
         <p>
             lorem ipsum
         </p>
-    </section>
+    </motion.section>
   )
 }
