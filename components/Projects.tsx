98cca52687c0c44d7e544cef83aed3cb1aeac335
sahilsethi99 <68042681+sahilsethi99@users.noreply.github.com>
2024-10-18 23:54:18 +0530

    config data for projects to be added

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,31 +1,7 @@
 import React from 'react'
 import SectionHeading from './SectionHeading'
 import Image from 'next/image';
-
-type ProjectProps=(typeof ok)[number];
-
-function Project({title,description,tags, imageUrl}:ProjectProps){
-    return (
-        <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]">
-           <div className="py-4 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] ">
-                <h3 className="text-2xl">
-                    {title}
-                </h3>
-                <p className="mt-2 leading-relaxed text-gray-700">
-
-                </p>
-                <ul className="flex flex-wrap mt-4 gap-2">
-                    {tags.map((tag,index)=>(
-                        <li className="bg-black/0.7 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
-                    ))}
-                </ul>
-
-           </div>
-            <Image src={imageUrl} alt={title} quality={95} className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"/>
-
-        </section>
-    );
-}
+import Project from './Project';
 
 export default function Projects() {
   return (
@@ -45,4 +21,4 @@ export default function Projects() {
   )
 }
 
-//paused at 2.34.20
+//paused at 2.59.54
