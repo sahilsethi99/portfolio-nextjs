fcc47bc30490a939bf42596fe327917206b06542
Sahil Sethi <sethisahil20132013@gmail.com>
2024-11-06 04:04:38 +0530

    added sendEmail server action

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,43 @@
 "use server";
 
+import { Resend } from "resend";
+const resend =new Resend(process.env.RESEND_API_KEY); 
+
+import { validateString } from "@/lib/utils";
+import { getErrorMessage } from "@/lib/utils";
+
+
 export const sendEmail =  async (formData:FormData) =>{
     console.log('Running on server');
     
-    console.log(formData.get("senderEmail"));
-    console.log(formData.get("message"));
+    const senderEmail = formData.get("senderEmail");
+    const message = formData.get("message");
+
+    //simple server-side validation
+    if(!validateString(senderEmail,500)){
+        return{
+            error:"Invalid sender email"
+        }
+    }
+
+    if(!validateString(message,5000)){
+        return{
+            error:"Invalid message"
+        }
+    }
+
+    try{
+        await resend.emails.send({
+            from:'Portfolio Contact Form <onboarding@resend.dev>',
+            to:'sethisahil20132013@gmail.com',
+            subject:'Important Message from Portfolio Contact Form',
+            reply_to: senderEmail as string,
+            text: message as string,
+        })
+
+    }catch(e:unknown){
+        return {
+            error: getErrorMessage(e),
+        };
+    }
 } 
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -24,13 +24,10 @@ export default function Contact() {
         <p className="text-gray-700 -mt-6">Got an idea or opportunity you'd like to discuss?</p>
           <p className="text-gray-700"> Contact me @{" "}<a className="underline" href="mailto:sethisahil20132013@gmail.com">sethisahil20132013@gmail.com</a>{" "}or through this form.</p>
         <form action={async (formData)=>{
-          console.log("Running on client");
-          console.log(formData.get("senderEmail"));
-          console.log(formData.get("message"));
           await sendEmail(formData);
         }} className="mt-10 flex flex-col">
         <input name="senderEmail" className="h-14 px-4  rounded-lg border border-black/10 " placeholder="Your email" type="email" required maxLength={500}/>
-        <textarea name="message" className="h-52 my-3 rounded-lg border border-black/10 p-4 " required maxLength={500} placeholder="Your message or any relevant details for our first meeting?"/>
+        <textarea name="message" className="h-52 my-3 rounded-lg border border-black/10 p-4 " required maxLength={5000} placeholder="Your message or any relevant details for our first meeting?"/>
         <button type="submit" className="group flex justify-center items-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105">Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1"/>{" "}</button>
         </form>
     </motion.section>
